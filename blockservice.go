2cac0eeb7b813ec31c4515bfa3dbf1b9e9b90c6c
Yang Zhao <jszyyx@163.com>
2021-05-07 16:14:13 +0800

    fix error

--- blockservice.go
+++ blockservice.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"sync"
 
+	crust "github.com/crustio/go-ipfs-encryptor/crust"
 	blocks "github.com/ipfs/go-block-format"
 	cid "github.com/ipfs/go-cid"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
@@ -263,12 +264,12 @@ func getBlock(ctx context.Context, c cid.Cid, bs blockstore.Blockstore, fget fun
 		// Seal
 		if root, err := crust.GetRootFromSealContext(ctx); err == nil {
 			bv := block.RawData()
-			needSeal, path, err := crust.Seal(root, false, bv)
+			needSeal, path, err := crust.Worker.Seal(root, false, bv)
 			if needSeal {
 				if err != nil {
 					return nil, err
 				}
-				err = bs.Put(NewWarpedSealedBlock(path, len(bv), c))
+				err = bs.Put(crust.NewWarpedSealedBlock(path, len(bv), c))
 				if err != nil {
 					return nil, err
 				}
